9dc0fa80a3ec533572172084890d976511bc7eb3
Garric81 <Borovichenkong@gmail.com>
2022-12-31 22:18:57 +0200

    create vo project sh scripts

---
+++ Install Docker Engine.sh
@@ -0,0 +1,3 @@
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#Verify that the Docker Engine installation is successful by running the hello-world image:
+sudo docker run hello-world
