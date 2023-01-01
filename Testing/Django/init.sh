07a937ab57287fa3d280614147de369360cc8233
Garric81 <Borovichenkong@gmail.com>
2023-01-01 11:13:48 +0200

    Createa web djanjo docker

---
+++ Testing/Django/init.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+
+# User credentials
+user=admin
+email=admin@example.com
+password=pass
+
+file=db/db.sqlite3
+
+#if [ -z "$file" ]; then
+#  echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+#fi
+python3 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
